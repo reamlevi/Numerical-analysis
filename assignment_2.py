e46b9454b231d2c659209ea013c8c109f9ec63c1
reamlevi <reamlevi@gmail.com>
2024-06-28 23:43:22 +0300

    add inverse function

--- assignment_2.py
+++ assignment_2.py
@@ -60,10 +60,60 @@ def smaller_matrix(matrix, row, col)-> list[list[int]]:
             smaller[smaller_raw].pop(col)  # remove the specified column
             smaller_raw += 1
     return smaller        
+
+def inverse(matrix)-> list[list[float]]:
+    """
+    Calculate the inverse of a square matrix.
+ 
+    Parameters:
+    matrix (list of lists): The square matrix.
+    
+    Returns:
+    list of lists: The inverse of the matrix.
+    """
+    num_rows = len(matrix)
+    identity_matrix = identity_calc(num_rows)
+
+    for i in range(num_rows):
+        # Divide the row by the pivot element
+        pivot = matrix[i][i]
+        for j in range(num_rows):
+            matrix[i][j] /= pivot
+            identity_matrix[i][j] /= pivot
+
+        # Subtract the pivot row from other rows
+        for k in range(num_rows):
+            if k != i:
+                factor = matrix[k][i]
+                for j in range(num_rows):
+                    matrix[k][j] -= factor * matrix[i][j]
+                    identity_matrix[k][j] -= factor * identity_matrix[i][j]
+
+    return identity_matrix
+
+def identity_calc(num_rows_and_cols):
+    """
+    This function generates an identity matrix of size num_rows_and_cols x num_rows_and_cols.
+
+    Parameters:
+    num_rows_and_cols (int): The number of rows and columns in the identity matrix.
+
+    Returns:
+    list of lists: A num_rows_and_cols x num_rows_and_cols identity matrix.
+    """
+    return [[float(i == j) for j in range(num_rows_and_cols)] for i in range(num_rows_and_cols)]
+ 
+
+
+
+
         
 
 A = [[1,-1,-2],[2,-3,-5],[-1,3,5]]
 
 print(square_matrix(A))
 print(determinant(A))
+
+print(inverse(A))
+
 
